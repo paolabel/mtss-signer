5d8081d7eb696486c11a8cdf64b8608c5181601a
paolabel <pa0la.abel@hotmail.com>
2023-03-11 15:17:46 -0300

    finished polyniomial cff builder, no tests

--- mtsssigner/cff_builder.py
+++ mtsssigner/cff_builder.py
@@ -7,34 +7,54 @@
 
 # imagem (resultados possíveis) de q = ????
 
+from typing import List
 import galois
 from galois import FieldArray
 from math import inf
-
-from utils import is_prime_power, get_cartesian_product, get_polynomials_with_deg_up_to_k
-
+import itertools
 import numpy
+import sys
+
+from utils import is_prime_power, get_all_coefficient_combinations_in_field, get_polynomials_with_deg_up_to_k, get_field_elements, print_polynomial_list, get_polynomial_value_at_x
+
 from numpy.polynomial import Polynomial
 
-def get_b_set(field: FieldArray, k: int) -> list[Polynomial]:
-    order: int = field.order
-    # assume que elementos do corpo finito sempre são "contínuos"
-    field_elements: list = [x for x in range(order)]
-    polinomial_ring_coeffs: list[list] = get_cartesian_product(field_elements)
+# TODO criar função de construção para 1-CFF 
+# e função que escolhe qual construção usar dependendo se d = 1 ou não
+
+
+def get_b_set(field: FieldArray, k: int) -> List[Polynomial]:
+    polinomial_ring_coeffs: list[list] = get_all_coefficient_combinations_in_field(field)
     polinomial_coeffs_with_deg_up_to_k = get_polynomials_with_deg_up_to_k(polinomial_ring_coeffs, k)
     b_set = [Polynomial(coefficients) for coefficients in polinomial_coeffs_with_deg_up_to_k]
     return b_set
 
-def create_cff(q: int, k: int):
+# transformar cada elemento em um Point?
+def get_x_set(field: FieldArray) -> List[list]:
+    field_elements: List[int] = get_field_elements(field)
+    result = itertools.product(field_elements, repeat=2)
+    return [tuple(element) for element in result]
+
+def create_polynomial_cff(q: int, k: int):
     assert is_prime_power(q)
     assert k >= 2
 
+    # talvez não precise checar se q é potência de primo pq o construtor da GF já o faz
     finite_field: FieldArray = galois.GF(q)
-    print(finite_field.properties)
+    b_set = get_b_set(finite_field, k)
+    x_set = get_x_set(finite_field)
     
+    cff_dimensions = (q**2, q**k)
+    cff = numpy.zeros(cff_dimensions, dtype=int)
     
+    for test in range(cff_dimensions[0]):
+        for block in range(cff_dimensions[1]):
+            if ((get_polynomial_value_at_x(b_set[block], x_set[test][0]) % q) == x_set[test][1]):
+                cff[test][block] = 1
+
+    return cff
+     
 if __name__ == '__main__':
-    sett = get_b_set(galois.GF(4), 3)
-    for element in sett:
-        print(str(element).replace('.0', '').replace('·', '').replace('x¹', 'x'))
+    numpy.set_printoptions(threshold=sys.maxsize)
+    print(create_polynomial_cff(3,2))
     
--- mtsssigner/utils.py
+++ mtsssigner/utils.py
@@ -1,6 +1,10 @@
 import math
 from itertools import chain, combinations
 import itertools
+from typing import List
+from numpy.polynomial import Polynomial
+import numpy
+from galois import FieldArray
 
 # https://geekflare.com/prime-number-in-python/
 def is_prime(number: int) -> bool:
@@ -33,15 +37,40 @@ def get_powerset(iterable: set) -> list:
     return chain.from_iterable(combinations(s, length) for length in range(len(s)+1))
 
 # https://stackoverflow.com/questions/533905/how-to-get-the-cartesian-product-of-multiple-lists
-def get_cartesian_product(iterable: list) -> list[list]:
+def get_all_coefficient_combinations_in_field(field: FieldArray) -> List[list]:
     # assume que sempre vai retornar a mesma ordem
-    result = itertools.product(iterable, repeat=len(iterable))
-    return [list(element) for element in result]
+    field_elements = get_field_elements(field)
+    result = itertools.product(field_elements, repeat=len(field_elements))
+    returnval = list()
+    for element in result:
+        coefs = list(element)
+        coefs.reverse()
+        returnval.append(coefs)
+    return returnval
 
-def get_polynomials_with_deg_up_to_k(polinomial_coefficients: list[list], k: int):
-    updated_polinomial_coefficients = list(list())
-    for polinomial_coefficient in polinomial_coefficients:
-        updated_polinomial_coefficient = polinomial_coefficient[:k]
-        if updated_polinomial_coefficient not in updated_polinomial_coefficients:
-            updated_polinomial_coefficients.append(updated_polinomial_coefficient)
-    return updated_polinomial_coefficients
+def get_polynomials_with_deg_up_to_k(polynomial_coefficients: List[list], k: int):
+    updated_polynomial_coefficients = list(list())
+    for polynomial_coefficient in polynomial_coefficients:
+        updated_polynomial_coefficient = polynomial_coefficient[:k]
+        if updated_polynomial_coefficient not in updated_polynomial_coefficients:
+            updated_polynomial_coefficients.append(updated_polynomial_coefficient)
+    return updated_polynomial_coefficients
+
+def get_polynomial_value_at_x(polynomial: Polynomial, x: int) -> int:
+    coefficients = list(polynomial.coef)
+    coefficients.reverse()
+    value = int(numpy.polyval(coefficients, x))
+    return value
+
+def get_field_elements(field: FieldArray) -> List[int]:
+    order: int = field.order
+    # assume que elementos do corpo finito sempre tem elementos contínuos
+    field_elements: list = [x for x in range(order)]
+    return field_elements
+
+def print_polynomial_list(polynomial_list: List[Polynomial]):
+    for element in polynomial_list:
+        print(str(element).replace('.0', '').replace('·', '').replace('x¹', 'x'))
+    
+if __name__ == '__main__':
+    print(get_all_coefficient_combinations_in_field([0,1,2,3]))
